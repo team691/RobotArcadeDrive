80f67cf3db2d58044e712530a5d7b694d2ae2747
Cole <cole900@att.net>
2022-01-22 09:11:23 -0800

    Fixed motot controll isssue and added Auto

---
+++ src/main/java/frc/AutoCommands.java
@@ -0,0 +1,5 @@
+package frc;
+
+public class AutoCommands {
+    
+}
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,10 +7,13 @@ package frc.robot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorController;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+//import java.util.Timer;
 
 //import edu.wpi.first.wpilibj.motorcontrol.CANSparkMax;
 import com.revrobotics.CANSparkMax;
@@ -26,16 +29,17 @@ public class Robot extends TimedRobot {
   private XboxController c;
  // private Joystick m_rightStick;
 
-  private final CANSparkMax m_leftMotor1 = new CANSparkMax(0, MotorType.kBrushed);
-  private final CANSparkMax m_leftMotor2 = new CANSparkMax(2, MotorType.kBrushed);
+  private final CANSparkMax m_leftMotor1 = new CANSparkMax(3, MotorType.kBrushed);
+  private final CANSparkMax m_leftMotor2 = new CANSparkMax(4, MotorType.kBrushed);
 
   MotorControllerGroup m_left = new MotorControllerGroup(m_leftMotor1, m_leftMotor2);
 
-  private final CANSparkMax m_rightMotor1 = new CANSparkMax(3, MotorType.kBrushed);
-  private final CANSparkMax m_rightMotor2 = new CANSparkMax(1, MotorType.kBrushed);
+  private final CANSparkMax m_rightMotor1 = new CANSparkMax(1, MotorType.kBrushed);
+  private final CANSparkMax m_rightMotor2 = new CANSparkMax(2, MotorType.kBrushed);
 
   MotorControllerGroup m_right = new MotorControllerGroup(m_rightMotor1, m_rightMotor2);
 
+  Timer m_timer = new Timer();
   @Override
   public void robotInit() {
     // We need to invert one side of the drivetrain so that positive voltages
@@ -51,11 +55,66 @@ public class Robot extends TimedRobot {
   
     //m_rightStick = new Joystick(1);
   }
+  @Override
+  public void autonomousInit(){
+    //Code for Auto period goes under this method
+   
+    /*
+    // Makes the robot go forward, turn around, then back
+   //m_myRobot.arcadeDrive(10, 0);
+   m_myRobot.arcadeDrive(-stick.getX(), stick.getY());
+   try{
+      wait(2);
+    } catch (InterruptedException e){
+    }
+    m_myRobot.arcadeDrive(0, 0);
+    m_myRobot.arcadeDrive(0, 180);
+    try{
+      wait(2);
+    } catch (InterruptedException e){
+    }
+    m_myRobot.arcadeDrive(10, 0);
+    try{
+      wait(2);
+    } catch (InterruptedException e){
+    }
+    m_myRobot.arcadeDrive(0, 0);
+
+    */
+    m_timer.reset();
+    m_timer.start();
+  }
+
+  @Override
+  public void autonomousPeriodic() {
+    // TODO Auto-generated method stub
+    super.autonomousPeriodic();
+    if(m_timer.get() < 10.0){
+      //m_myRobot.arcadeDrive(0.5, 0.0);
+     //goForward();
+     m_left.set(0.25);
+     m_right.set(-0.25);
+     
+    }
+    //m_left.set(-0.1);
+    //m_right.set(0.1);
+   
+    else{
+      m_myRobot.stopMotor();
+      
+    }
+  }
+  public void goForwad(){
+    m_left.set(0.1);
+    m_right.set(-0.1);
+  }
 
   @Override
   public void teleopPeriodic() {
     m_myRobot.arcadeDrive(-stick.getX(), stick.getY());
-  // m_left.set(stick.getY() + stick.getX());
+  
+  
+     //m_left.set(stick.getY() + stick.getX());
    //m_left.set(stick.getY() + stick.getX());
    //m_right.set(stick.getY() - stick.getX());
     //SmartDashboard.putNumber("X", stick.getX());
